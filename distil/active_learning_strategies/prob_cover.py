8c32a678369da60a843fa08e57e33375ab505a56
ofer1992 <geon.arcan@gmail.com>
2022-01-30 16:45:17 +0200

    prob cover and resnet modifications for simclr

--- distil/active_learning_strategies/prob_cover.py
+++ distil/active_learning_strategies/prob_cover.py
@@ -6,6 +6,9 @@ from .strategy import Strategy
 class ProbCover(Strategy):
     def __init__(self, labeled_dataset, unlabeled_dataset, net, nclasses, args={}) -> None:
         super(ProbCover, self).__init__(labeled_dataset, unlabeled_dataset, net, nclasses, args)
+        self.k = args['k']
+        self.delta = args['delta']
+        self.update_model(args['embedding'])
 
 
     def densest_first(self, unlabeled_embeddings, labeled_embeddings, n):
@@ -16,13 +19,11 @@ class ProbCover(Strategy):
         # if labeled_embeddings.shape[0] == 0:
             # min_dist = torch.tile(float("inf"), m)
         # else:
-        k = 50 # TODO put as arg
-        delta = 2.
         dist_ctr = torch.cdist(unlabeled_embeddings, labeled_embeddings, p=2)
-        density = torch.exp(-torch.sort(dist_ctr, dim=1)[:,:k].mean(dim=1))
-        to_zero = torch.any(dist_ctr.min(dim=1) < delta, dim=0)
+        density = torch.exp(-torch.sort(torch.cdist(unlabeled_embeddings, unlabeled_embeddings, p=2), dim=1)[0][:,:self.k+1].mean(dim=1))
+        to_zero = dist_ctr.min(dim=1)[0] < self.delta
         density[to_zero] = 0.
-
+        # import pdb; pdb.set_trace()
         idxs = []
         
         for i in range(n):
@@ -30,7 +31,7 @@ class ProbCover(Strategy):
             idxs.append(idx.item())
             dist_new_ctr = torch.cdist(unlabeled_embeddings, unlabeled_embeddings[[idx],:])
             # min_dist = torch.minimum(min_dist, dist_new_ctr[:,0])
-            to_zero = torch.any(dist_new_ctr.min(dim=1) < delta, dim=0)
+            to_zero = dist_new_ctr.min(dim=1)[0] < self.delta
             density[to_zero] = 0
                 
         return idxs
@@ -52,3 +53,5 @@ class ProbCover(Strategy):
         embedding_unlabeled = self.get_embedding(self.unlabeled_dataset)
         embedding_labeled = self.get_embedding(NoLabelDataset(self.labeled_dataset))
         chosen = self.densest_first(embedding_unlabeled, embedding_labeled, budget)
+
+        return chosen
--- distil/utils/models/resnet.py
+++ distil/utils/models/resnet.py
@@ -10,7 +10,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -18,6 +17,7 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
@@ -29,10 +29,10 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = self.relu(out)
         return out
 
 
@@ -71,12 +71,15 @@ class ResNet(nn.Module):
         self.in_planes = 64
         self.embDim = 8 * self.in_planes * block.expansion
         
-        self.conv1 = nn.Conv2d(channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(channels, self.in_planes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1,1))
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
 
@@ -91,20 +94,20 @@ class ResNet(nn.Module):
     def forward(self, x, last=False, freeze=False):
         if freeze:
             with torch.no_grad():
-                out = F.relu(self.bn1(self.conv1(x)))
+                out = self.maxpool(self.relu(self.bn1(self.conv1(x))))
                 out = self.layer1(out)
                 out = self.layer2(out)
                 out = self.layer3(out)
                 out = self.layer4(out)
-                out = F.avg_pool2d(out, 4)
+                out = self.avgpool(out)
                 e = out.view(out.size(0), -1)
         else:
-            out = F.relu(self.bn1(self.conv1(x)))
+            out = self.maxpool(self.relu(self.bn1(self.conv1(x))))
             out = self.layer1(out)
             out = self.layer2(out)
             out = self.layer3(out)
             out = self.layer4(out)
-            out = F.avg_pool2d(out, 4)
+            out = self.avgpool(out)
             e = out.view(out.size(0), -1)
         out = self.linear(e)
         if last:
