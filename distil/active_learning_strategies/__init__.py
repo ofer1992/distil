d10c7e476cc93f9188f6bf4069211a497aeb8abf
ofer1992 <geon.arcan@gmail.com>
2022-02-21 18:44:12 +0200

    commented out sampling methods that require numba

--- distil/active_learning_strategies/__init__.py
+++ distil/active_learning_strategies/__init__.py
@@ -6,7 +6,7 @@ from .bayesian_active_learning_disagreement_dropout import BALDDropout
 from .core_set import CoreSet
 from .entropy_sampling import EntropySampling
 from .entropy_sampling_dropout import EntropySamplingDropout
-from .fass import FASS
+# from .fass import FASS
 from .glister import GLISTER
 from .gradmatch_active import GradMatchActive
 from .kmeans_sampling import KMeansSampling
@@ -16,8 +16,9 @@ from .margin_sampling import MarginSampling
 from .margin_sampling_dropout import MarginSamplingDropout
 from .partition_strategy import PartitionStrategy
 from .random_sampling import RandomSampling
-from .scg import SCG
-from .scmi import SCMI
-from .smi import SMI
-from .submod_sampling import SubmodularSampling
+# from .scg import SCG
+# from .scmi import SCMI
+# from .smi import SMI
+# from .submod_sampling import SubmodularSampling
 from .prob_cover import ProbCover
+from .gmm_coreset import GMMCoreset
